510deef5c36d05275da1b3f5b7f4394f0c4d3c6d
Nyikwagh Moses <mosnyik@gmail.com>
2026-03-01 18:43:35 +0100

    fix: attempt to fix the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,12 +29,8 @@ export const metadata: Metadata = {
     type: "website",
   },
   icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
-    apple: [{ url: "/apple-touch-icon.png" }],
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
 };
 
